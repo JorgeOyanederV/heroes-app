ea55540a120befa9dfa7b92915301d901803459a
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-04-18 17:35:55 -0400

    Pantalla para Buscar Heroes por Nombre.

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,55 @@
+import queryString from 'query-string'
+import { memo, useMemo } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useForm } from "../../helpers/useForm"
+import { getHeroesByName } from "../../selectors/getHeroesByName"
+import { HeroCard } from "../hero/HeroCard"
 
 export const SearchScreen = () => {
+   const location = useLocation();
+   const { q = '' } = queryString.parse(location.search);
+   const navigation = useNavigate();
+   const [formValues, handleInputChange] = useForm({
+      name: q
+   })
+   const { name } = formValues;
+   const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      navigation(`?q=${name}`)
+   }
    return (
-      <div>
-         <h1>SearchScreen
-         </h1>
+      <div className="row">
+         <div className="col-4">
+            <label>Buscar Heroe</label>
+            <hr />
+            <form onSubmit={handleSubmit}>
+               <input
+                  type="text"
+                  autoComplete="off"
+                  onChange={handleInputChange}
+                  name="name"
+                  className="form-control"
+                  value={name}
+               />
+               <button
+                  type="submit"
+                  className="btn btn-outline-primary mt-2"
+               >
+                  Buscar
+               </button>
+            </form>
+
+
+         </div>
+         <div className="col-8">
+            {
+               heroesFiltered.map(heroe =>
+                  <HeroCard {...heroe} key={heroe.id} />
+               )
+            }
+         </div>
       </div>
    )
 }
