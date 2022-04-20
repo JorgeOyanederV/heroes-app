02375fe6f9d86362da1cf47a75a7f4de69224f14
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-04-20 08:44:03 -0400

    Correccion UI

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -5,10 +5,10 @@ import { HeroCard } from './HeroCard';
 export const HeroList = ({ publisher }) => {
    const heroes = useMemo(() => getHeroeByPublisher(publisher), [publisher]);
    return (
-      <div className='row'>
+      <div className='row justify-content-between'>
          {
             heroes.map(hero => (
-               <div className='col-12 col-md-4' key={hero.id}>
+               <div className='col-12 col-lg-4 mb-3' key={hero.id}>
                   <HeroCard  {...hero} />
                </div>
             ))
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -3,16 +3,20 @@ import { DcScreen } from "../components/dc/DcScreen"
 import { HeroScreen } from "../components/hero/HeroScreen"
 import { MarvelScreen } from "../components/marvel/MarvelScreen"
 import { SearchScreen } from "../components/search/SearchScreen"
+import { Navbar } from "../components/ui/Navbar"
 
 export const DashboardRoutes = () => {
    return (
-      <div className="mt-3">
-         <Routes>
-            <Route exact path='marvel' element={<MarvelScreen />} />
-            <Route exact path='dc' element={<DcScreen />} />
-            <Route exact path='search' element={<SearchScreen />} />
-            <Route exact path='hero/:id' element={<HeroScreen />} />
-         </Routes>
-      </div>
+      <>
+         <Navbar />
+         <div className="container mt-3">
+            <Routes>
+               <Route exact path='marvel' element={<MarvelScreen />} />
+               <Route exact path='dc' element={<DcScreen />} />
+               <Route exact path='search' element={<SearchScreen />} />
+               <Route exact path='hero/:id' element={<HeroScreen />} />
+            </Routes>
+         </div>
+      </>
    )
 }
